7bbc4406c962d6b805916597e1693401c2f6469b
Andrew Tran <avtran23@gmail.com>
2017-01-23 13:35:25 -0800

    Install dependencies, enable sessions in server
    Dependency 'flash' is for login/signup pop-ups

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('connect-flash');
 require('../db');
 
 var bodyParser = require('body-parser');
@@ -7,6 +10,13 @@ var router = require('./routes');
 var app = express();
 app.use(bodyParser.json());
 app.use('/api', router);
+app.use(cookieParser());
+app.use(session({ 
+    secret: 'trainrecc',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
 
 var port = 3000;
 app.listen(port, function(){
