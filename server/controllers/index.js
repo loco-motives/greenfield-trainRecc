c5eecaf8d3d1bd0facdc4ac8a23f4b9e81c2d668
Andrew Tran <avtran23@gmail.com>
2017-01-23 14:50:45 -0800

    Add 'post' route
    Add 'post' route to 'routes.js' and 'controllers/index.js'

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,44 @@
+var bcrypt = require('bcryptjs'),
+    userModel = require('../../db/index.js').User;
+
 var users = {
     get: function(req, res){
         console.log('Serving request for ', req.method, 'where url is ', req.url);
         res.send('got');
     }
 };
+
+var signup = {
+    // get: function(req, res){
+    //     res.render('/signup');
+    // }
+    post: function(req, res){
+        var username = req.body.username;
+        var password = req.body.password;
+
+        if(!username || !password) {
+            req.flash('error', 'Please fill out all fields');
+            // res.redirect('signup');
+        }
+
+        var salt = bcrypt.genSaltSync(10);
+        var hashedPassword = bcrypt.hashSync(password, salt);
+
+        var newUser = {
+            username: username,
+            salt: salt,
+            password: hashedPassword
+        }
+
+        Model.user.create(newUser).then( () => {
+            res.redirect('/');
+        }). catch( (err) => {
+            req.flash('error', 'Please choose a different username');
+            // res.redirect('/signup');
+        });
+    }
+}
 module.exports = {
-    users: users
+    users: users,
+    signup: signup
 };
--- server/routes.js
+++ server/routes.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 var controller = require('./controllers');
 
 router.get('/users', controller.users.get);
+router.post('/signup', signupController.signup.post);
 
 module.exports = router;
