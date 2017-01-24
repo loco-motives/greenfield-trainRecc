0600217430662f41114a1a19c47469a2ac0b601d
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-01-24 15:26:04 -0800

    Resolve merge conflict in routes.js

--- server/routes.js
+++ server/routes.js
@@ -4,18 +4,12 @@ var passport = require('passport');
 
 router.get('/users', controller.users.get);
 router.post('/signup', controller.signup.post);
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.post('/login', passport.authenticate('local', {
     successRedirect:'/',
     failureRedirect: '/login',
     failureFlash: true
 }));
-=======
-router.post('/hypemSongs', controller.hypemSongs.post);
->>>>>>> Start on parsing each song from user search
-=======
+
 router.post('/hypemSongs', controller.findHypemSongs.post);
->>>>>>> Download most relevant song when user inputs song search query
 
 module.exports = router;
