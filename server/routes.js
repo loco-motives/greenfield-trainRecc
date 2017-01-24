67e5dfcd690d20a60b86270d1bc68cf898e6ba54
Andrew Tran <avtran23@gmail.com>
2017-01-23 18:43:47 -0800

    Implement login functionality
    Looks as though un/successful logins are working, can confirm after
    we have redirect, login, and signup pages implemented.

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,13 @@
 var router = require('express').Router();
 var controller = require('./controllers');
+var passport = require('passport');
 
 router.get('/users', controller.users.get);
 router.post('/signup', controller.signup.post);
+router.post('/login', passport.authenticate('local', {
+    successRedirect:'/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
 
 module.exports = router;
