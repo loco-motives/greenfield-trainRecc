069b7033d5efeeaef8650b6d9b1d760385631090
Andrew Tran <avtran23@gmail.com>
2017-01-25 11:22:53 -0800

    Implement signup component and template

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SignupService } from './signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  inputUsername: string;
+  inputPassword: string;
 
-  constructor() { }
+  constructor(private signUpService: SignupService) { }
 
   ngOnInit() {
+  }
+
+  submitSignup = () => {
+    this.signUpService.signUp(this.inputUsername, this.inputPassword)
+    .subscribe(res => {
+      this.inputUsername = '';
+      this.inputPassword = '';
+    }, err => {
+      console.log('err', err);
+    });
   }
 
 }
