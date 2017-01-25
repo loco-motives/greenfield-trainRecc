314dfe8295dc5d8ee0d3584694de30844765c8a5
Andrew Tran <avtran23@gmail.com>
2017-01-25 12:44:09 -0800

    Create login component and service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import {trigger, state, animate, style, transition} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class LoginComponent implements OnInit {
+  loginUsername: string;
+  loginPassword: string;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
