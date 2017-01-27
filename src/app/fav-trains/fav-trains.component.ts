ae6a3e35b6170789a3a176101f2229ed0f660c3d
Andrew Tran <avtran23@gmail.com>
2017-01-27 14:36:34 -0800

    Attempt to make player show

--- src/app/fav-trains/fav-trains.component.ts
+++ src/app/fav-trains/fav-trains.component.ts
@@ -11,12 +11,16 @@ export class FavTrainsComponent implements OnInit {
   public trains;
 
   constructor(private getTrainsService: GetTrainsService, private sanitizer: DomSanitizer ) { }
-
   ngOnInit() {
     this.getTrainsService.getTrains()
       .subscribe(res => {
         console.log('res.json is: ', res.json());
         this.trains = res.json();
+        this.trains.forEach(trainObj => {
+          trainObj.songs.forEach(songObj => {
+            songObj.songSourcePath = this.sanitize(songObj.songSourcePath);
+          });
+        });
         console.log('this.trains:',this.trains)
       });
   }
