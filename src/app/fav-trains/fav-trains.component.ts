9c261d1257761a29a1a28098052a0f6757ce5e49
Andrew Tran <avtran23@gmail.com>
2017-01-27 14:36:34 -0800

    Attempt to make player show

--- src/app/fav-trains/fav-trains.component.ts
+++ src/app/fav-trains/fav-trains.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { GetTrainsService } from '../services/get-trains.service';
+import {DomSanitizer} from '@angular/platform-browser';
+
 @Component({
   selector: 'app-fav-trains',
   templateUrl: './fav-trains.component.html',
@@ -8,13 +10,22 @@ import { GetTrainsService } from '../services/get-trains.service';
 export class FavTrainsComponent implements OnInit {
   public trains;
 
-  constructor(private getTrainsService: GetTrainsService ) { }
+  constructor(private getTrainsService: GetTrainsService, private sanitizer: DomSanitizer ) { }
+
+  sanitize(path) {
+    return this.sanitizer.bypassSecurityTrustUrl(path);
+  }
 
   ngOnInit() {
     this.getTrainsService.getTrains()
       .subscribe(res => {
         console.log('res.json is: ', res.json());
         this.trains = res.json();
+        this.trains.forEach(trainObj => {
+          trainObj.songs.forEach(songObj => {
+            songObj.songSourcePath = this.sanitize(songObj.songSourcePath);
+          });
+        });
         console.log('this.trains:',this.trains)
       });
   }
