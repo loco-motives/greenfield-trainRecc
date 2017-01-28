4e3447367570fb8b96e8db71f92db08baa1b6cda
Andrew Tran <avtran23@gmail.com>
2017-01-27 19:01:32 -0800

    Create addSongToTrain service

--- src/app/services/add-song-to-train.service.ts
+++ src/app/services/add-song-to-train.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class AddSongToTrainService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
+
+  addSong(songObj, trainId) {
+    var body = {};
+    body.trainId = trainId;
+    body.songTitle = songObj.song;
+    body.songId = songObj.id;
+    body.songKey = song.key;
+    body.songArtist = songObj.artist;
+    //what was songImg again?
+    return _http.post('/api/addsongtotrain', body);
+  }
 
 }
